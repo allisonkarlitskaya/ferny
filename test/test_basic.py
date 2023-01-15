b85631f0d9c4159405266ae260e37ee261c0a941
Martin Pitt <martin@piware.de>
2023-01-15 17:40:47 +0100

    test: Fix secret key permissions
    git does not track file permissions, and files have mode 0644 by default
    (at least with a standard 022 umask). This upsets SSH:
    
        RuntimeError: b'Warning: Permanently added \'[127.0.0.1]:40847\' (RSA) to the list of known hosts.\r\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n@         WARNING: UNPROTECTED PRIVATE KEY FILE!          @\r\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\nPermissions 0644 for \'test/id_rsa.enc\' are too open.\r\nIt is required that your private key files are NOT accessible by others.\r\nThis private key will be ignored.\r\nLoad key "test/id_rsa.enc": bad permissions\r\nadmin@127.0.0.1: Permission denied (publickey).\r\n'
    
    Copy them to a temp directory and chmod them to 600 for the test.

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,5 +1,8 @@
+import glob
 import os
 import unittest
+import shutil
+import tempfile
 
 import mockssh
 
@@ -18,6 +21,16 @@ class MockAskpass(ferny.Askpass):
 
 
 class TestBasic(unittest.IsolatedAsyncioTestCase):
+    @classmethod
+    def setUpClass(kls):
+        # git does not track file permissions, and SSH fails on group/world readability
+        kls.key_dir = tempfile.TemporaryDirectory(prefix='ferny-test-keys.')
+        # copy all test/id_* files to the temporary directory with 0600 permissions
+        for key in glob.glob('test/id_*'):
+            dest = os.path.join(kls.key_dir.name, os.path.basename(key))
+            shutil.copy(key, dest)
+            os.chmod(dest, 0o600)
+
     async def test_rsa_key(self):
         users = {'admin': 'test/id_rsa'}
         with mockssh.Server(users) as server:
@@ -26,7 +39,7 @@ class TestBasic(unittest.IsolatedAsyncioTestCase):
                                   port=server.port,
                                   configfile='none',
                                   options={'UserKnownHostsFile': '/dev/null'},
-                                  identity_file='test/id_rsa.enc',
+                                  identity_file=os.path.join(self.key_dir.name, 'id_rsa.enc'),
                                   login_name='admin',
                                   askpass_factory=MockAskpass)
             print('done!')
