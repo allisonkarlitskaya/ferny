allisonkarlitskaya/ferny

commit 0017f9b603fd3612b8cf7785f357032203ab5451

session: fix deadlock when interaction responder throws
If an interaction responder throws an exception then it successfully
aborts the running interaction.  That doesn't always exit SSH, though —
in the case of a password prompt, for example, we'll get asked a second
time.  In that case, the agent is no longer responding to requests and
SSH will wait on us forever at the same time that we're waiting on SSH
to quit.

The cleanest and easiest thing to do here is to SIGKILL ssh.  We could
close our side of the agent socket, which would prevent the deadlock in
this case, but only by causing future agent attempts to fail.  In some
cases if SSH keeps asking us (like when getting an invalid response
about whether to accept a hostkey) we may end up in a livelock
situation.

src/ferny/session.py
@@ -153,6 +153,11 @@ class Session(SubprocessContext, InteractionResponder):
             assert os.path.exists(self._controlsock)
             self._process = process
         except BaseException:
+            # If we get here because the InteractionResponder raised an
+            # exception then SSH might still be running, and may even attempt
+            # further interactions (ie: 2nd attempt for password).  We already
+            # have our exception and don't need any more info.  Kill it.
+            process.kill()
             await process.wait()
             raise
 

commit b48ded594b4264fbaa718f485ca1339ce3280f31

session: catch ProcessLookupError on .kill()
0017f9b603 introduced a .kill() call on the ssh process if
authentication failed, in order to avoid a deadlock.

Even though the subprocess code goes out of its way not to emit a
ProcessLookupError, the asyncio code will explicitly raise it if it
knows that the child process has already exited.

This shows up from time to time in our unit tests since the last commit.

In any case, let's just catch and ignore the exception.

src/ferny/session.py
@@ -157,7 +157,10 @@ class Session(SubprocessContext, InteractionResponder):
             # exception then SSH might still be running, and may even attempt
             # further interactions (ie: 2nd attempt for password).  We already
             # have our exception and don't need any more info.  Kill it.
-            process.kill()
+            try:
+                process.kill()
+            except ProcessLookupError:
+                pass  # already exited?  good.
             await process.wait()
             raise
 